abc9a87628b1b8b419887735e1b57b930e20f31a
Elizabeth Bryson <liz@lizbryson.co>
2021-07-29 10:02:25 -0500

    add mdx

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
     "gatsby-plugin-image",
     "gatsby-plugin-sitemap",
     "gatsby-plugin-sharp",
+    "gatsby-plugin-mdx",
     "gatsby-transformer-sharp",
     {
       resolve: "gatsby-source-filesystem",
